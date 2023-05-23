3db5a3adb90d3e28f49f9f6b5c4b4bec55860e33
Your Name~ <davidjamieson1985@gmail.com>
2023-05-24 01:24:05 +1200

    up

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 
@@ -12,28 +12,33 @@ console.log(document.querySelector('.guess').value);
 */
 
 const secretnumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretnumber;
+document.querySelector(".number").textContent = secretnumber;
 let score = 20;
-document.querySelector('.check').addEventListener
-('click', function() {
-    const guess = Number(document.querySelector('.guess').value);
-    console.log(guess, typeof guess);
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
 
-    if (!guess) {
-        document.querySelector('.message').textContent = 
-        '⛔ No Number!';
-     } else if (guess === secretnumber) {
-        document.querySelector('.message').textContent =
-        '🍾 Correct Number!';
-     } else if (guess > secretnumber) {
-        document.querySelector('.message').textContent =
-        '📈 Too High!';
-        for (score)?????; 
-        document.querySelector('.score').textContent = score;
-     } else if (guess < secretnumber) {
-        document.querySelector('.message').textContent =
-        '📉 Too Low!';
-        score--; 
-        document.querySelector('.score').textContent = score;
-     }
+  if (!guess) {
+    document.querySelector(".message").textContent = "⛔ No Number!";
+  } else if (guess === secretnumber) {
+    document.querySelector(".message").textContent = "🍾 Correct Number!";
+  } else if (guess > secretnumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📈 Too High!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = " You're a Loser!!";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < secretnumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📉 Too Low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = " You're a Loser!!";
+      document.querySelector(".score").textContent = 0;
+    }
+  }
 });

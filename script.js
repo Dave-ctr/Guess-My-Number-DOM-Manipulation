3c130f56f370859de17897796f9d2c19a11cd9f4
Dave-ctr <83482840+Dave-ctr@users.noreply.github.com>
2023-05-23 16:08:40 +1200

    up

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+/*console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 
-'🍾 Correct Number!';
+'🍾 Correct secretNumber!';
 
-document.querySelector('.number').textContent = 13;
+document.querySelector('.secretnumber').textContent = 13;
 document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
 
-const number = Math.random();
-
-const x = function() {
-    console.log(23);
-}
-
+const secretnumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretnumber;
+let score = 20;
 document.querySelector('.check').addEventListener
 ('click', function() {
-    const guess = document.querySelector('guess').value
-    console.log(guess, typeof '.guess');
-});
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
 
-document.querySelector('.message').textContent = 
-'🍾 Correct Number!';
-
-if (!'guess') {
-    document.querySelector('.message').textContent = 
-'😠 No Number!';
- }
+    if (!guess) {
+        document.querySelector('.message').textContent = 
+        '⛔ No Number!';
+     } else if (guess === secretnumber) {
+        document.querySelector('.message').textContent =
+        '🍾 Correct Number!';
+     } else if (guess > secretnumber) {
+        document.querySelector('.message').textContent =
+        '📈 Too High!';
+        for (score)?????; 
+        document.querySelector('.score').textContent = score;
+     } else if (guess < secretnumber) {
+        document.querySelector('.message').textContent =
+        '📉 Too Low!';
+        score--; 
+        document.querySelector('.score').textContent = score;
+     }
 });
